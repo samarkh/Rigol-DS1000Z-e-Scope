c5a835a91b35f65c8dfb06b7d1ffc9796af94042
samarkh <simonmarkham@hotmail.com>
2025-06-23 10:33:11 +0100

    tweek

---
+++ Measurements/Measurements.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS1000Z_E_USB_Control.Measurements
+{
+    internal class Measurements
+    {
+    }
+}
